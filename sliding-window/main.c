ade837d0966b1cd2cb649db34eaac5aec5ed92e5
Karl Xu <karl.yogi.xu@gmail.com>
2020-11-06 11:42:05 +0800

    sliding window algorithm: use generateRandomNumbers() to generate certain number of integers within certain rangeg

--- sliding-window/main.c
+++ sliding-window/main.c
@@ -3,17 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-void printRandom(int lower, int upper) {
-    if (upper < lower) {
-        fprintf(stderr,"printRandom(): upper parameter should not be smaller than lower parameter.");
-    }
-    int number = rand() % (upper - lower + 1);
+int* generateRandomNumbers(int lower, int upper, int count) {
+    int random_numbers[count];
+    for (int i = 0; i < count; i++) {
+        if (upper < lower) {
+            fprintf(stderr,"printRandom(): upper parameter should not be smaller than lower parameter.");
+        }
+        int number = rand() % (upper - lower + 1);
 
-    printf("random number: %d\n",number );
+        random_numbers[i] = number;
+
+        // printf("random number: %d\n",number );
+    }
+
+    return random_numbers;
+    
 }
 
 
 int main() {
-    printRandom(1,10);
+    int lower = 1, upper = 10, count=5;
+    int* random_numbers = generateRandomNumbers(1,10, 5);
+
+    for(int i=0; i < count; i++) {
+        printf("Number %d: %d\n", i+1, random_numbers[i]);
+    }
     return 0;
 }
