e67cbe518e102f441bf8ddb81753f6557ee3c050
Karl Xu <karl.yogi.xu@gmail.com>
2020-11-06 13:28:04 +0800

    sliding window: try out simple solution to find biggest sums

---
+++ sliding-window/simple-algorithm.c
@@ -0,0 +1,55 @@
+// This program use nested loop to find smallest sum in an integer array.
+// Build command: gcc -o simple-algorithm ./simple-algorithm.c
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <limits.h>
+
+int* generateRandomNumbers(int lower, int upper, int count) {
+    int random_numbers[count];
+    for (int i = 0; i < count; i++) {
+        if (upper < lower) {
+            fprintf(stderr,"printRandom(): upper parameter should not be smaller than lower parameter.");
+        }
+        int number = rand() % (upper - lower + 1);
+
+        random_numbers[i] = number;
+
+        // printf("random number: %d\n",number );
+    }
+
+    return random_numbers;
+    
+}
+
+
+int main()
+{  
+    int integers[9] = {1, 4, 2, 10, 23, 3, 1, 0, 20};
+    int array_size = sizeof(integers)/sizeof(integers[0]);
+
+
+    printf("array byte size: %d\narray length: %d\n", (int)sizeof(integers), array_size);
+
+
+    printf("\nStart calculation...\n");
+    int max_sum = INT_MIN;
+    int sum_size = 2;
+
+    for(int i=0; i < array_size; i++) {
+
+        int biggest_starting_point_for_sum = array_size - sum_size + 1;
+        int current_sum = 0;
+        for (int j =i; j < biggest_starting_point_for_sum; j++ ) {
+            current_sum += integers[j];
+        }
+
+        max_sum = (max_sum > current_sum) ? max_sum : current_sum;
+        printf("%d time calculation: %d\n", i+1, max_sum);
+    }
+
+    printf("\nThe biggest sequential sum of %d integer(s) is %d\n", sum_size, max_sum);
+
+    return 0;
+}
+
