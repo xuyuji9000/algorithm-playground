a09b6a1fef9fe490bb5fc6ca52aad737b727e8e9
Karl Xu <karl.yogi.xu@gmail.com>
2020-11-06 10:14:34 +0800

    sliding window: random number generation within range

--- sliding-window/Readme.md
+++ sliding-window/Readme.md
@@ -2,3 +2,10 @@ This folder is for implementing sliding window algorithm.
 
 
 - Build command: `gcc main.c -o main`
+
+
+
+# Reference
+
+
+1. [Window Sliding Technique](https://www.geeksforgeeks.org/window-sliding-technique/)
--- sliding-window/main.c
+++ sliding-window/main.c
@@ -1,7 +1,19 @@
+// This program is used to practice sliding window algorithm.
+
 #include <stdio.h>
+#include <stdlib.h> 
+
+void printRandom(int lower, int upper) {
+    if (upper < lower) {
+        fprintf(stderr,"printRandom(): upper parameter should not be smaller than lower parameter.");
+    }
+    int number = rand() % (upper - lower + 1);
+
+    printf("random number: %d\n",number );
+}
 
 
 int main() {
-    printf("Hello World.\n");
+    printRandom(1,10);
     return 0;
 }
