3f475eab1fccdab6afbf41b6755efc67a6861fcd
Karl Xu <karl.yogi.xu@gmail.com>
2020-11-08 11:27:02 +0800

    huffman coding: read character frequency input from a txt file

--- huffman-coding/main.c
+++ huffman-coding/main.c
@@ -2,6 +2,7 @@
 // Build command: gcc -o main ./main.c
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Characters {
     char value;
@@ -10,15 +11,28 @@ struct Characters {
 
 int main() {
 
-    // printf("Hello World.\n");
-    struct Characters char1;
-    
-    char1.value='a';
-    char1.frequency=5;
+    FILE *fptr;
+    char file_path[] ="./characters.txt";
 
-    printf("Character \'%c\' frequency: %d\n", char1.value, char1.frequency);
-     
+    fptr = fopen(file_path, "r");
 
+    if(NULL == fptr) {
+        fprintf(stderr, "Error! opening file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t numbers_read; //[1]
+    char * line = NULL;
+    size_t len = 0;
+    while(-1 != (numbers_read = getline(&line, &len,fptr))) {
+        printf("Line length: %zu\nLine value: %s\n", numbers_read, line);
+    }
 
     return 0;
 }
+
+
+/*
+# Reference
+1. [What is ssize_t in C? ](https://jameshfisher.com/2017/02/22/ssize_t/)
+*/
