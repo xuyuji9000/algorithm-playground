bf92fb5d989b2d3732b7adf30e5199b18bcbfa93
Karl Xu <karl.yogi.xu@gmail.com>
2020-11-08 15:50:12 +0800

    huffman coding: get charater value and frequency from txt file

--- huffman-coding/main.c
+++ huffman-coding/main.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Characters {
     char value;
@@ -11,27 +12,48 @@ struct Characters {
 
 int main() {
 
-    FILE *fptr;
+    FILE *file_pointer;
     char file_path[] ="./characters.txt";
 
-    fptr = fopen(file_path, "r");
+    file_pointer = fopen(file_path, "r");
 
-    if(NULL == fptr) {
+    if(NULL == file_pointer) {
         fprintf(stderr, "Error! opening file\n");
         exit(EXIT_FAILURE);
     }
 
+    /*
+      Fixing the array size here, can be convert to dynamic array size.
+    */
+    struct Characters Characters_array[10]; 
     ssize_t numbers_read; //[1]
-    char * line = NULL;
+    char * line_content = NULL;
     size_t len = 0;
-    while(-1 != (numbers_read = getline(&line, &len,fptr))) {
-        printf("Line length: %zu\nLine value: %s\n", numbers_read, line);
+
+    int line_number = 0;
+    while(-1 != (numbers_read = getline(&line_content, &len,file_pointer))) {
+
+        /*
+
+        */
+        char delemimator = ',';
+        char* value = strtok(line_content, &delemimator); // [2]
+
+        int frequency = atoi(strtok(NULL, &delemimator)); // [3]
+
+        printf("Line length: %zu\nCharacter value: %s\nCharacter frequency: %d\n", numbers_read, value, frequency);
+
+        
+
+        // Characters_array[line_number].;
+        
+        line_number++;
     }
 
-    fclose(fptr);
+    fclose(file_pointer);
 
-    if (line)
-        free(line);
+    if (line_content)
+        free(line_content);
     exit(EXIT_SUCCESS);
 }
 
@@ -39,4 +61,9 @@ int main() {
 /*
 # Reference
 1. [What is ssize_t in C? ](https://jameshfisher.com/2017/02/22/ssize_t/)
+
+2. [C library function - strtok()](https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm)
+
+3. [C library function - atoi()](https://www.tutorialspoint.com/c_standard_library/c_function_atoi.htm)
 */
+
