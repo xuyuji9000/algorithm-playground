cd1848208ba89575acf9cf742f621c31916abaac
Karl Xu <karl.yogi.xu@gmail.com>
2020-11-11 18:54:29 +0800

    huffman coding: add internal node logic and remove legacy code

--- huffman-coding/main.c
+++ huffman-coding/main.c
@@ -129,10 +129,10 @@ int main() {
             break;
         }
 
-        // struct Node* internal_node = (struct Node*)malloc(sizeof(struct Node));
-        // // createInternalNode( internal_node, first_node, second_node );
+        struct Node* internal_node = (struct Node*)malloc(sizeof(struct Node));
+        createInternalNode( internal_node, first_node, second_node );
 
-        // // insert(myMinHeap, *internal_node);
+        insert(myMinHeap, *internal_node);
 
 
         // Free not using pointers
@@ -140,79 +140,9 @@ int main() {
         first_node=NULL;
         free(second_node);
         second_node=NULL;  
-        // free(internal_node);
-        // internal_node=NULL;  
+        free(internal_node);
+        internal_node=NULL;  
     }
-
-    
-
-    
-
-    /*
-      Fixing the array size here, can be convert to dynamic array size.
-    */
-/*
-    struct Node Node_array[10]; 
-    ssize_t numbers_read; //[1]
-    char * line_content = NULL;
-    size_t len = 0;
-
-    int line_number = 0;
-    while(-1 != (numbers_read = getline(&line_content, &len,file_pointer))) {
-
-        char delemimator = ',';
-        Node_array[line_number].value =  strtok(line_content, &delemimator)[0]; // [2]
-
-        Node_array[line_number].frequency =  atoi(strtok(NULL, &delemimator)); // [3]
-
-        // DEBUG:
-        // printf("Line length: %zu\nCharacter value: %c\nCharacter frequency: %d\n\n", numbers_read, Node_array[line_number].value, Node_array[line_number].frequency);
-        
-        line_number++;
-    }
-*/
-
-/*
-    int Node_count = line_number;
-    int Node_index=0;
-
-    struct Node* root;
-
-    struct Node* new_Node = malloc(sizeof(struct Node));
-    createInternalNode( &new_Node, &Node_array[0], &Node_array[1] );
-
-
-    // printf("New internal Node frequency: %d\n", (*new_Node).frequency);
-
-    root = new_Node;
-    new_Node = NULL; // Prevent dangling pointer
-    Node_index += 2;
-
-    int end_of_Node_index = Node_count - 1;
-
-    while(Node_index <= end_of_Node_index) {
-        int Nodes_left = (end_of_Node_index - Node_index) +1;
-
-        struct Node* new_Node = malloc(sizeof(struct Node));
-
-        if( 1 == Nodes_left ) {
-            if ( (*root).frequency > Node_array[Node_index].frequency)
-                createInternalNode(&new_Node, root, &Node_array[Node_index]);
-            else
-                createInternalNode(&new_Node, &Node_array[Node_index], root);
-            root = new_Node;            
-            printf("New internal Node frequency: %d\n", (*new_Node).frequency);
-            break;
-        }
-
-        Node_index++;
-           
-
-        
-
-    }
-*/
-
 
     fclose(file_pointer);
     if (line_content)
